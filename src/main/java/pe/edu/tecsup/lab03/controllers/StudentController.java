51b35a3cc7080d201c3628d5895533678ea19be3
Angie-67 <angievasquezgg@gmail.com>
2026-09-01 23:33:22 -0500

    Recuperar cambios de Student

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,7 +1,8 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
-    public void listarEstudiantes() {
-        System.out.println("Lista de estudiantes");
+
+    public void mostrarMensaje() {
+        System.out.println("Controlador de estudiantes");
     }
 }
--- src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
+++ src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
@@ -1,7 +1,4 @@
 package pe.edu.tecsup.lab03.services;
 
 public class StudentService {
-    public void registrarEstudiante() {
-        System.out.println("Registrando estudiante...");
-    }
 }
