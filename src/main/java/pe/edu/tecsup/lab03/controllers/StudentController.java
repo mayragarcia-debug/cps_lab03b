7fd1e4a37ffd13532242856b9c5eb4c14e12e8cd
mayragarcia-debug <mayra.garcia@tecsup.edu.pe>
2026-09-01 23:30:36 -0500

    Agregar método mostrarNombre

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -5,4 +5,8 @@ public class StudentController {
     public void mostrarMensaje() {
         System.out.println("Controlador de estudiantes");
     }
+
+    public void mostrarNombre() {
+        System.out.println("Sistema de estudiantes");
+    }
 }
